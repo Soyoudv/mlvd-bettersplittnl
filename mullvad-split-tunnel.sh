2b0f7415f0c7599fa518c6e37084b6ec0c1dbc9c
Soyoudv <noname54765@gmail.com>
2025-12-30 00:02:12 +0100

    implémentation du log, nettoyage des echo

--- mullvad-split-tunnel.sh
+++ mullvad-split-tunnel.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#reset log
+: > ./out.log
+
 EXCLUDED_APPS="./excluded-apps"
 echo "Using excluded apps list from: $EXCLUDED_APPS"
 
@@ -7,7 +10,6 @@ echo "Using excluded apps list from: $EXCLUDED_APPS"
 mapfile -t EXCLUDED_APPS < "$EXCLUDED_APPS"
 
 STATE_FILE="$HOME/.cache/mullvad-split-pids"
-echo "Started using state file: $STATE_FILE"
 
 #ensure state file directory exists
 mkdir -p "$(dirname "$STATE_FILE")"
@@ -15,12 +17,20 @@ mkdir -p "$(dirname "$STATE_FILE")"
 #reset state file
 : > "$STATE_FILE" # : because > alone can fail if noclobber is set
 
+echo "Cleaning up existing split tunnel pids..."
+
+mullvad split-tunnel clear >> ./out.log
+
+echo "Split tunnel running, ${#EXCLUDED_APPS[*]} apps excluded."
+
 while true; do
   for app in "${EXCLUDED_APPS[@]}"; do
+    # echo "Checking for app: $app"
+
     pgrep -f "$app" | while read -r pid; do
       if ! grep -q "^$pid$" "$STATE_FILE"; then
-        echo -e "\e[31m$app\e[0m [$pid]"
-        if mullvad split-tunnel add "$pid"; then
+        echo -e "$app [$pid]" >> ./out.log
+        if mullvad split-tunnel add "$pid" >> ./out.log; then
           echo "$pid" >> "$STATE_FILE"
         else 
           echo "Failed to add $app [$pid] to split tunnel"
